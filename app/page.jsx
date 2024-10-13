7087cf97716a00cbbedc26addb83d2fc8c0af2bf
ahmetberktas <ahmetberktas50@gmail.com>
2024-10-13 20:11:30 +0300

    Add HomePageContainer

--- app/page.jsx
+++ app/page.jsx
@@ -1,3 +1,5 @@
+import HomePageContainer from "@/containers/home";
+
 export default function Home() {
-  return <div>Home</div>;
+  return <HomePageContainer />;
 }
---
+++ containers/home/index.jsx
@@ -0,0 +1,16 @@
+import styles from "./styles.module.css";
+
+function HomePageContainer() {
+  return (
+    <div className={styles.homePageContainer}>
+      <h1 className={styles.title}>
+        Just Imagine, <br /> We Take Care of the Rest
+      </h1>
+      <p className={styles.description}>
+        Tell us a prompt and we&apos;ll generate a story for you.
+      </p>
+    </div>
+  );
+}
+
+export default HomePageContainer;
