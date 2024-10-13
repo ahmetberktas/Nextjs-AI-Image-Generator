6de9e05b0de89382d3a1af87f52ef11b43bb7498
ahmetberktas <ahmetberktas50@gmail.com>
2024-10-13 19:58:45 +0300

    Add Head component and integrate Next.js Font library

---
+++ app/head.jsx
@@ -0,0 +1,18 @@
+function Head() {
+  return (
+    <>
+      <title>Imaginer - AI Image Generator</title>
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <meta
+        name="description"
+        content="Imaginer is a simple AI image generator that creates stunning visuals based on your prompts."
+      />
+      <meta
+        name="keywords"
+        content="AI, image generator, prompts, art, technology"
+      />
+    </>
+  );
+}
+
+export default Head;
--- app/layout.jsx
+++ app/layout.jsx
@@ -2,9 +2,14 @@ import "../styles/reset.css";
 import "../styles/variable.css";
 import "../styles/global.css";
 
+import { inter } from "@/lib/font";
+
+import Head from "./head";
+
 export default function RootLayout({ children }) {
   return (
-    <html lang="tr">
+    <html lang="tr" className={inter.className}>
+      <Head></Head>
       <body className="layout">
         {children}
         <div className="overlay"></div>
